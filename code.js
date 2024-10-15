72a5934f19b34365957cf2980f811f781eeb11b5
Quincy Morris Reynolds <143751384+qdog307@users.noreply.github.com>
2024-10-15 09:43:06 -0600

    First

--- code.js
+++ code.js
@@ -1,3 +1,25 @@
 function divideAndConquerSum(a) {
-    return a;
+    
+    if (a.length === 1) {
+        return a[0];
+    }
+
+    
+    if (a.length === 0) {
+        return 0;
+    }
+
+    
+    const third = Math.floor(a.length / 3);
+    const firstPart = a.slice(0, third);
+    const secondPart = a.slice(third, 2 * third);
+    const thirdPart = a.slice(2 * third);
+
+    
+    const sumFirst = divideAndConquerSum(firstPart);
+    const sumSecond = divideAndConquerSum(secondPart);
+    const sumThird = divideAndConquerSum(thirdPart);
+
+    
+    return sumFirst + sumSecond + sumThird;
 }
